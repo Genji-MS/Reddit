7a4531ad662d6193aa43c808ec841171344c631b
Genji-MS <Genji.Tapia@students.makeschool.com>
2020-06-26 13:30:37 -0700

    fixed posts test, added subreddit req to test. And thereby fixed comments route to display names.

--- controllers/comments.js
+++ controllers/comments.js
@@ -2,24 +2,53 @@ const Comment = require('../models/comment');
 const Post = require('../models/post');
 const User = require('../models/user');
 
-module.exports = function(app) {
-    // CREATE Comment
-    app.post("/posts/:postId/comments", function(req, res) {
-    // INSTANTIATE INSTANCE OF MODEL
-        var currentUser = req.user;
+// Braus' fix to remove redundant .then. But not working?
+// module.exports = function(app) {
+//     // CREATE Comment
+//     app.post("/posts/:postId/comments", function(req, res) {
+//     // INSTANTIATE INSTANCE OF MODEL
+//         var currentUser = req.user;
         
-        const comment = new Comment(req.body);
-        comment.author = req.user._id;
-        comment
-            .save()
-            .then(comment => { 
-                const post = Post.findById(req.params.postId);
-                post.comments.unshift(comment);
-                post.save();
-                return res.redirect(`/posts/${req.params.postId}`);
-            })
-            .catch(err => {
-            console.log(err);
-            });
-    });
+//         const comment = new Comment(req.body);
+//         comment.author = req.user._id;
+//         comment
+//             .save()
+//             .then(comment => { 
+//                 const post = Post.findById(req.params.postId);
+//                 post.comments.unshift(comment);
+//                 post.save();
+//                 return res.redirect(`/posts/${req.params.postId}`);
+//             })
+//             .catch(err => {
+//             console.log(err);
+//             });
+//     });
+// };
+
+// tutorial version
+module.exports = function (app) {
+    // CREATE Comment
+    app.post("/posts/:postId/comments", function (req, res) {
+      const comment = new Comment(req.body);
+      comment.author = req.user._id;
+      comment
+          .save()
+          .then(comment => {
+              return Promise.all([
+                  Post.findById(req.params.postId)
+              ]);
+          })
+          .then(([post, user]) => {
+              post.comments.unshift(comment);
+              return Promise.all([
+                  post.save()
+              ]);
+          })
+          .then(post => {
+              res.redirect(`/posts/${req.params.postId}`);
+          })
+          .catch(err => {
+              console.log(err);
+          });
+  });
 };
